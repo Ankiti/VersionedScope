412e4bcef8662ad6bd8fa18270bb09ffdcf434f9
Ankiti Aggarwal <ankiti1234@gmail.com>
2023-11-15 20:36:09 -0500

    VersionedScope class

---
+++ tests.py
@@ -0,0 +1,111 @@
+from versionScope import VersionedScope
+from datetime import datetime
+
+def run_tests():
+    # Test Case 1: Basic usage
+    scope = VersionedScope()
+
+    scope.set('name', 'John')
+    scope.set('age', 25)
+    version1 = scope.commit()
+
+    scope.set('name', 'Jane')
+    scope.set('city', 'New York')
+    version2 = scope.commit()
+
+    
+    assert scope.properties == {'name': 'Jane', 'age': 25, 'city': 'New York'}
+    
+    assert scope.get_property_version('name') == version2
+    assert scope.get_property_version('city') == version2
+
+    # Test Case 2: Conflict resolution
+    scope.set('name', 'Jack')
+    scope.set('age', 30)
+    version3 = scope.commit()
+
+    scope.set('name', 'Jake')
+    scope.set('city', 'Los Angeles')
+    version4 = scope.commit()
+
+    assert scope.properties == {'name': 'Jake', 'age': 30, 'city': 'Los Angeles'}
+    assert scope.get_property_version('name') == version4
+    assert scope.get_property_version('city') == version4
+
+    # Test Case 3: Rollback
+    scope.rollback(version3)
+ 
+    assert scope.properties == {'name': 'Jack', 'age': 30, 'city': 'New York'}
+    
+    assert scope.get_property_version('name') == version3
+    assert scope.get_property_version('city') == version2
+
+    # Test Case 4: Query historical states
+    history = scope.query_history()
+    assert len(history) == 4  # Four transactions (initial, commit1, commit2, rollback)
+
+    point_in_time = scope.history[1]['timestamp']
+    history_at_point = scope.query_history(point_in_time=point_in_time)
+    assert history_at_point == scope.history[1]
+
+    transactions_prior = 2
+    history_prior = scope.query_history(transactions_prior=transactions_prior)
+    assert len(history_prior) == transactions_prior
+
+    # Test Case 5: Error handling
+    try:
+        scope.rollback(0)  # Invalid version number
+    except ValueError as e:
+        assert str(e) == "Invalid version number"
+
+    try:
+        scope.rollback(10)  # Version doesn't exist
+    except ValueError as e:
+        assert str(e) == "Invalid version number"
+
+    try:
+        scope.commit()  # No changes to commit
+    except ValueError as e:
+        assert str(e) == "No changes to commit"
+
+
+    # Test Case 6: Conflicting deletes
+    new_scope = VersionedScope()
+
+    new_scope.set('name', 'John')
+    new_scope.set('age', 25)
+    version1 = new_scope.commit()
+
+    new_scope.delete('name')
+    new_scope.set('city', 'New York')
+    version2 = new_scope.commit()
+
+    assert new_scope.properties == {'age': 25, 'city': 'New York'}
+    assert new_scope.get_property_version('name') is None
+    assert new_scope.get_property_version('city') == version2
+
+    # Test Case 7: Multiple rollbacks
+    new_scope.rollback(version1)
+    assert new_scope.properties == {'name': 'John', 'age': 25}
+    assert new_scope.get_property_version('name') == version1
+    assert new_scope.get_property_version('city') is None
+
+    new_scope.rollback(version2)
+  
+    assert new_scope.properties == {'age': 25, 'city': 'New York'}
+    assert new_scope.get_property_version('name') is None
+    assert new_scope.get_property_version('city') == version2
+
+    # Test Case 8: Query history with non-existent timestamp
+    assert new_scope.query_history(point_in_time=datetime(2022, 1, 1)) is None
+
+    # Test Case 9: Query history with invalid transactions_prior
+    assert new_scope.query_history(transactions_prior=0) == new_scope.query_history()
+
+    print("All test cases passed!")
+
+
+
+# Run the tests
+run_tests()
+
---
+++ versionScope.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from collections import defaultdict
+
+class VersionedScope:
+    def __init__(self):
+        self.properties = {}
+        self.transactions = {}
+        self.history = []
+        self.pending_transaction = defaultdict(dict)
+
+    def set(self, key, value):
+        self.pending_transaction[key] = {'action': 'set', 'value': value}
+
+    def delete(self, key):
+        self.pending_transaction[key] = {'action': 'delete'}
+
+    def commit(self):
+        if self.pending_transaction:
+            timestamp = datetime.now()
+            version = len(self.transactions) + 1
+
+            # Check for conflicts
+            conflicts = self.detect_conflicts()
+          
+            if conflicts:
+                # Resolve conflicts (overwrite with the latest change)
+                for conflict in conflicts:
+                    key = conflict['key']
+                    latest_change = conflict['latest_change']
+                    self.pending_transaction[key] = latest_change
+
+            # Apply changes to properties
+            for key, change in self.pending_transaction.items():
+                if change['action'] == 'set':
+                    self.properties[key] = change['value']
+                elif change['action'] == 'delete' and key in self.properties:
+                    del self.properties[key]
+
+            # Record the transaction in history
+            self.transactions[version] = {'timestamp': timestamp, 'changes': dict(self.pending_transaction)}
+            self.history.append(self.transactions[version])
+
+            # Clear pending_transaction for the next transaction
+            self.pending_transaction = defaultdict(dict)
+
+            return version
+        else:
+            raise ValueError("No changes to commit")
+
+    def detect_conflicts(self):
+        conflicts = []
+        for key, change in self.pending_transaction.items():
+            if key in self.properties:
+                conflicts.append({'key': key, 'latest_change': self.pending_transaction[key]})
+        return conflicts
+
+    def rollback(self, version):
+        if 1 <= version <= len(self.transactions):
+            # Revert properties to the specified version
+            self.properties = {}
+            for v in range(1, version + 1):
+                for key, change in self.transactions[v]['changes'].items():
+                    if change['action'] == 'set':
+                        self.properties[key] = change['value']
+                    elif change['action'] == 'delete' and key in self.properties:
+                        del self.properties[key]
+                
+        else:
+            raise ValueError("Invalid version number")
+
+    def get_property_version(self, key):
+        if key not in self.properties:
+            return None
+
+        value = self.properties[key]
+    
+        for version, transaction in reversed(self.transactions.items()):
+           
+            if key in transaction['changes'] and transaction['changes'][key]['action'] == 'set':
+                if value == transaction['changes'][key]['value']:
+                    return version
+        
+
+    def query_history(self, point_in_time=None, transactions_prior=None):
+        if point_in_time:
+            return next((entry for entry in self.history if entry['timestamp'] == point_in_time), None)
+        elif transactions_prior:
+            start_version = max(1, len(self.transactions) - transactions_prior + 1)
+            return [self.transactions[i] for i in range(start_version, len(self.transactions) + 1)]
+        else:
+            return self.history
+
+
